db844c1beab9e32600dfce0132022c2de6514dec
Lucivando Sousa <lucivandosousa@gmail.com>
2024-05-06 19:11:35 -0300

    feat: adicionar projeto

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
+import { useState } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Image, SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import { styles } from './style';
 
 export default function App() {
+
+  const [cep, setCep] = useState("")
+  const [address, setAddress] = useState({})
+
+  function handleInputCep(texto) {
+    setCep(texto)
+  }
+
+  async function consultarCep() {
+
+    const regexCep = /^[0-9]{8}$/.test(cep)
+
+    if (cep.length < 8 || !regexCep) {
+      Alert.alert("CEP", "Digite um CEP válido!")
+      return
+    }
+
+    const request = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    const json = await request.json()
+
+    setAddress({ ...json })
+
+    setCep("")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+
+      <Image
+        source={require("./assets/logo_correios.png")}
+      />
+
+      <View style={styles.viewResult}>
+
+        <Text style={styles.title}>Consulta CEP</Text>
+
+        <TextInput
+          placeholder='Digite o CEP...'
+          style={styles.textInput}
+          onChangeText={handleInputCep}
+          value={cep}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={consultarCep}
+        >
+          <Text style={styles.textButton}>Consultar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setAddress({})}
+        >
+          <Text style={styles.textButton}>Limpar Resultado</Text>
+        </TouchableOpacity>
+
+        <View >
+          <Text style={styles.resultTextTitle}>Resultado da Busca</Text>
+          <Text style={styles.resultText}>{address.logradouro}</Text>
+          <Text style={styles.resultText}>{address.bairro}</Text>
+          <Text style={styles.resultText}>{address.localidade} - {address.uf}</Text>
+        </View>
+
+      </View>
+
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ style.js
@@ -0,0 +1,51 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffd400',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10
+  },
+  viewResult: {
+    backgroundColor: "#D1CCC7",
+    padding: 20,
+    borderRadius: 10
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "500",
+    textAlign: "center",
+    color: "#0071ad",
+    marginBottom: 10
+  },
+  textInput: {
+    backgroundColor: "#ececec",
+    padding: 15,
+    width: "width",
+    borderRadius: 5,
+    fontSize: 20
+  },
+  button: {
+    marginTop: 10,
+    height: 60,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#0071ad"
+  },
+  textButton: {
+    color: "#ffffff"
+  },
+  resultTextTitle: {
+    fontSize: 30,
+    color: "#0071ad",
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  resultText: {
+    fontSize: 20,
+    color: "#0071ad"
+  }
+})
